e3b9f6e463fd323f6739d496b99b6ecaa7455ade
Chaitanya Shetty <chaitanyashetty47@gmail.com>
2024-11-15 17:39:58 +0530

    trigger deploymemt

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -8,7 +8,7 @@ export async function POST(request: NextRequest) {
   const { name, surname, email, country, dialCode, contactNumber, appointmentDate, appointmentTime, servicesInterested } = await request.json();
 
   try {
-    // Create a new contact record in the database with servicesInterested
+    // Create a new contact record in the database with servicesInterestedd
     const newContact = await prisma.contactSubmission.create({
       data: {
         name,
